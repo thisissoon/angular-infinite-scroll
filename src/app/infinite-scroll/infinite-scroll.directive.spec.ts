9b19668a4e79c13fec94ba296d7e50a6e057c7a9
Ed Opare-Aryee <ed@thisissoon.com>
2018-12-03 18:05:53 +0000

    build(yarn): use yarn instead of npm

--- src/app/infinite-scroll/infinite-scroll.directive.spec.ts
+++ src/app/infinite-scroll/infinite-scroll.directive.spec.ts
@@ -23,36 +23,30 @@ describe('InfiniteScrollDirective', () => {
     expect(spy).toHaveBeenCalledWith({ y: 200, height: 300 });
   });
 
-  it(
-    'should emit event scrollEnd event',
-    fakeAsync(() => {
-      const spy = spyOn(directive.scrollEnd, 'emit');
+  it('should emit event scrollEnd event', fakeAsync(() => {
+    const spy = spyOn(directive.scrollEnd, 'emit');
 
-      directive.onScroll(199, 300, 100);
-      tick(100);
-      expect(spy).not.toHaveBeenCalled();
+    directive.onScroll(199, 300, 100);
+    tick(100);
+    expect(spy).not.toHaveBeenCalled();
 
-      directive.onScroll(200, 300, 100);
-      tick(100);
-      expect(spy).toHaveBeenCalled();
-    })
-  );
+    directive.onScroll(200, 300, 100);
+    tick(100);
+    expect(spy).toHaveBeenCalled();
+  }));
 
-  it(
-    'should emit event scrollEnd event with offset',
-    fakeAsync(() => {
-      const spy = spyOn(directive.scrollEnd, 'emit');
-      directive.offset = 50;
+  it('should emit event scrollEnd event with offset', fakeAsync(() => {
+    const spy = spyOn(directive.scrollEnd, 'emit');
+    directive.offset = 50;
 
-      directive.onScroll(149, 300, 100);
-      tick(100);
-      expect(spy).not.toHaveBeenCalled();
+    directive.onScroll(149, 300, 100);
+    tick(100);
+    expect(spy).not.toHaveBeenCalled();
 
-      directive.onScroll(150, 300, 100);
-      tick(100);
-      expect(spy).toHaveBeenCalled();
-    })
-  );
+    directive.onScroll(150, 300, 100);
+    tick(100);
+    expect(spy).toHaveBeenCalled();
+  }));
 
   it('should unsubscribe from observable on destroy lifecycle hook', () => {
     const spy = spyOn(directive.scrollEnd, 'emit');
